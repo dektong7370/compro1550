cff2d2c42f3f073cde4a4fe91000bb00170ad9cd
kritsadainkaew@gmail.com <kritsadainkaew@gmail.com>
2012-06-19 15:53:46 +0700

    HelloWorld

---
+++ src/th/ac/nu/students/u54341550/compro/ch01/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package th.ac.nu.students.u54341550.compro.ch01;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello,World!");
+        
+    }
+}
